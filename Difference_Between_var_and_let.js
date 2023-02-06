2175b654b0c0fd5e579a50eeb80b25045cfa9f6e
Chris Azuaje <christopherazuaje@gmail.com>
2023-02-06 03:57:50 -0800

    Add files via upload

---
+++ Difference_Between_var_and_let.js
@@ -0,0 +1,9 @@
+//The problem with var is that it can easily be overwritten which can cause bugs 
+
+var camper = "James";
+var camper = "David";
+
+//As opposed to let which can't be overwritten once declared
+
+let van = "Bob";
+let van = "Howard"
