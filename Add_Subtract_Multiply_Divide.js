677447899b5e7978112fc5da2fdd8d2e073d0512
Chris Azuaje <christopherazuaje@gmail.com>
2023-02-07 02:46:46 -0800

    Add files via upload

---
+++ Add_Subtract_Multiply_Divide.js
@@ -0,0 +1,11 @@
+//The code below shows how to create basic math functions using JavaScript
+
+const sum = 10 + 5;
+const difference = 30 - 5;
+const product = 2 * 3;
+const quotient = 30 / 10;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
