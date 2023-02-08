a8c6e74118a40f7da2eee9dd69385a2effc03f25
Chris Azuaje <christopherazuaje@gmail.com>
2023-02-08 02:24:52 -0800

    Add files via upload

---
+++ Incrementing_and _Decrimenting.js
@@ -0,0 +1,9 @@
+//The below code shows how to increment in js
+let myVar = 87;
+myVar++;
+console.log(myVar);
+
+//The below code shows how to decriment in js
+let yourVar = 25;
+yourVar--;
+console.log(yourVar);
