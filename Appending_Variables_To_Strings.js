5c4160ecb86b0080e3b4dcc72db2c1354eacce00
Chris Azuaje <christopherazuaje@gmail.com>
2023-02-18 16:00:54 -0800

    Appending variables to strings

---
+++ Appending_Variables_To_Strings.js
@@ -0,0 +1,5 @@
+// The code below shows how 2 strings can be concatinated to create a new string.
+
+const someAdjective = "amazing";
+let myStr = "Learning to code is ";
+console.log(myStr += someAdjective);
