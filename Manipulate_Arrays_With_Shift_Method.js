bad90d79bd25823e93594ca3edbd2de7b5c11b56
Chris Azuaje <christopherazuaje@gmail.com>
2023-02-25 14:58:08 -0800

    Using the shift method

---
+++ Manipulate_Arrays_With_Shift_Method.js
@@ -0,0 +1,5 @@
+// This code shows how the shift method removes the first array 
+
+const myArray = [["John", 23], ["dog", 3]];
+const removedFromMyArray = myArray.shift();
+console.log(removedFromMyArray);
